7953e9cb98014dd28ba001262f148fffef5642bf
CathyCahy <cathycahy@gmail.com>
2024-05-20 22:34:52 +0300

    homework done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
+        Car[] cars = {new Car("car1", 4), new Car("car2", 4)};
+        Truck[] trucks = {new Truck("truck1", 6), new Truck("truck2", 8)};
+        Bicycle[] bicycles = {new Bicycle("bicycle1", 2), new Bicycle("bicycle2", 2)};
 
+        ServiceStation station = new ServiceStation();
+        station.check(cars[0], null, null);
+        station.check(cars[1], null, null);
+        station.check(null, bicycles[0], null);
+        station.check(null, bicycles[1], null);
+        station.check(null, null, trucks[0]);
+        station.check(null, null, trucks[1]);
     }
 }
